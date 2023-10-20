a2414eb7240e6943ab6a48ddcfd48307429be563
Manish Khedekar <mankhedekar96@gmail.com>
2023-10-21 04:27:37 +0530

    Test added for single string parameter in the function

--- stringCalculator/index.test.js
+++ stringCalculator/index.test.js
@@ -4,4 +4,7 @@ describe('String calculator', () => {
     it('returns 0 if got empty string as parameter', () => {
         expect(add('')).toEqual(0);
     });
+    it('returns same number if got single number string as parameter', () => {
+        expect(add('1')).toEqual(1);
+    });
 });
