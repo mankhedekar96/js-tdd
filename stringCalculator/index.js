1839e0482f7519105757b86e480c61d3b0fb3381
Manish Khedekar <mankhedekar96@gmail.com>
2023-10-21 13:36:49 +0530

    Test fixed for multiple numbers string with delimiters

--- stringCalculator/index.js
+++ stringCalculator/index.js
@@ -2,8 +2,16 @@ function add(str) {
     // when string is empty
     if(str === '') return 0;
 
+    // check if string has delimiter
+    let delimiter;
+    if(str.includes('//')) {
+        delimiter = str.split('\n')[0][2];
+        str = str.split('\n')[1];
+    }
+    if(delimiter) str = str.replaceAll(delimiter, ',');
+
     // check if string has \n and replace it with ,
-    if(str.includes('\n')) str = str.replace('\n', ',');
+    if(str.includes('\n')) str = str.replaceAll('\n', ',');
 
     // spliting string into array using , delimiter
     const arrayOfNumbers = str.split(',');
