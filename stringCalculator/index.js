31765eed4ade2625e185823261a336e4c5fba4db
Manish Khedekar <mankhedekar96@gmail.com>
2023-10-21 13:16:48 +0530

    Test fixed for multiple numbers string with commas and new line character

--- stringCalculator/index.js
+++ stringCalculator/index.js
@@ -2,6 +2,9 @@ function add(str) {
     // when string is empty
     if(str === '') return 0;
 
+    // check if string has \n and replace it with ,
+    if(str.includes('\n')) str = str.replace('\n', ',');
+
     // spliting string into array using , delimiter
     const arrayOfNumbers = str.split(',');
     if(arrayOfNumbers.length === 1) return Number(arrayOfNumbers[0]);
