2d1c730b80d91b4a7d936fc1b987e65e36507967
Manish Khedekar <mankhedekar96@gmail.com>
2023-10-21 12:42:53 +0530

    Test added for multiple numbers string with commas as parameter

--- stringCalculator/index.js
+++ stringCalculator/index.js
@@ -1,6 +1,7 @@
 function add(str) {
     if(str === '') return 0;
     if(Number(str) !== NaN) return Number(str);
+    return str.split(',').reduce((acc = 0, el) => acc += Number(el))
 }
 
 export default add;
